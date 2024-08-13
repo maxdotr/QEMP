2526da2a46e5993a08507ee8cd9280ffde42b30b
maxdotr <maxdotrodgers@gmail.com>
2024-08-13 13:31:39 -0600

    Query fixes
    Removing unnecessary tilda keys that negatively affected the capabilities of the library. Also added scrubbing for reserved mysql words, just in case keys or table names are similar. For example. a column named "group" now gets cleaned.

--- QEMP.php
+++ QEMP.php
@@ -39,6 +39,44 @@
  * @updated    May 17th, 2024
  */
 
+ /*********************************   RESERVED WORDS  ***************************************/
+ /******************************************************************************************/
+ $mysql_reserved_words = array(
+    "ADD", "ALL", "ALTER", "ANALYZE", "AND", "AS", "ASC", "ASENSITIVE", "BEFORE", "BETWEEN", 
+    "BIGINT", "BINARY", "BLOB", "BOTH", "BY", "CALL", "CASCADE", "CASE", "CHANGE", "CHAR", 
+    "CHARACTER", "CHECK", "COLLATE", "COLUMN", "CONDITION", "CONSTRAINT", "CONTINUE", 
+    "CONVERT", "CREATE", "CROSS", "CUBE", "CUME_DIST", "CURRENT_DATE", "CURRENT_TIME", 
+    "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "DATABASE", "DATABASES", "DAY_HOUR", 
+    "DAY_MICROSECOND", "DAY_MINUTE", "DAY_SECOND", "DEC", "DECIMAL", "DECLARE", "DEFAULT", 
+    "DELAYED", "DELETE", "DENSE_RANK", "DESC", "DESCRIBE", "DETERMINISTIC", "DISTINCT", 
+    "DISTINCTROW", "DIV", "DOUBLE", "DROP", "DUAL", "EACH", "ELSE", "ELSEIF", "EMPTY", 
+    "ENCLOSED", "ESCAPED", "EXCEPT", "EXISTS", "EXIT", "EXPLAIN", "FALSE", "FETCH", "FIRST_VALUE", 
+    "FLOAT", "FLOAT4", "FLOAT8", "FOR", "FORCE", "FOREIGN", "FROM", "FULLTEXT", "FUNCTION", 
+    "GENERATED", "GET", "GRANT", "GROUP", "GROUPING", "GROUPS", "HAVING", "HIGH_PRIORITY", 
+    "HOUR_MICROSECOND", "HOUR_MINUTE", "HOUR_SECOND", "IF", "IGNORE", "IN", "INDEX", "INFILE", 
+    "INNER", "INOUT", "INSENSITIVE", "INSERT", "INT", "INT1", "INT2", "INT3", "INT4", "INT8", 
+    "INTEGER", "INTERVAL", "INTO", "IO_AFTER_GTIDS", "IO_BEFORE_GTIDS", "IS", "ITERATE", 
+    "JOIN", "JSON_TABLE", "KEY", "KEYS", "KILL", "LAG", "LAST_VALUE", "LATERAL", "LEAD", 
+    "LEADING", "LEAVE", "LEFT", "LIKE", "LIMIT", "LINEAR", "LINES", "LOAD", "LOCALTIME", 
+    "LOCALTIMESTAMP", "LOCK", "LONG", "LONGBLOB", "LONGTEXT", "LOOP", "LOW_PRIORITY", "MASTER_BIND", 
+    "MASTER_SSL_VERIFY_SERVER_CERT", "MATCH", "MAXVALUE", "MEDIUMBLOB", "MEDIUMINT", "MEDIUMTEXT", 
+    "MIDDLEINT", "MINUTE_MICROSECOND", "MINUTE_SECOND", "MOD", "MODIFIES", "NATURAL", "NOT", 
+    "NO_WRITE_TO_BINLOG", "NTH_VALUE", "NTILE", "NULL", "NUMERIC", "OF", "ON", "OPTIMIZE", 
+    "OPTIMIZER_COSTS", "OPTION", "OPTIONALLY", "OR", "ORDER", "OUT", "OUTER", "OUTFILE", 
+    "OVER", "PARTITION", "PERCENT_RANK", "PRECISION", "PRIMARY", "PROCEDURE", "PURGE", 
+    "RANGE", "RANK", "READ", "READS", "READ_WRITE", "REAL", "RECURSIVE", "REFRENCE", "REGEXP", 
+    "RELEASE", "RENAME", "REPEAT", "REPLACE", "REQUIRE", "RESIGNAL", "RESTRICT", "RETURN", 
+    "REVOKE", "RIGHT", "RLIKE", "ROW", "ROWS", "ROW_NUMBER", "SCHEMA", "SCHEMAS", "SECOND_MICROSECOND", 
+    "SELECT", "SENSITIVE", "SEPARATOR", "SET", "SHOW", "SIGNAL", "SMALLINT", "SPATIAL", 
+    "SPECIFIC", "SQL", "SQLEXCEPTION", "SQLSTATE", "SQLWARNING", "SQL_BIG_RESULT", "SQL_CALC_FOUND_ROWS", 
+    "SQL_SMALL_RESULT", "SSL", "STARTING", "STORED", "STRAIGHT_JOIN", "SYSTEM", "TABLE", 
+    "TERMINATED", "THEN", "TINYBLOB", "TINYINT", "TINYTEXT", "TO", "TRAILING", "TRIGGER", 
+    "TRUE", "UNDO", "UNION", "UNIQUE", "UNLOCK", "UNSIGNED", "UPDATE", "USAGE", "USE", "USING", 
+    "UTC_DATE", "UTC_TIME", "UTC_TIMESTAMP", "VALUES", "VARBINARY", "VARCHAR", "VARCHARACTER", 
+    "VARYING", "VIRTUAL", "WHEN", "WHERE", "WHILE", "WINDOW", "WITH", "WRITE", "XOR", 
+    "YEAR_MONTH", "ZEROFILL"
+);
+
  /*********************************   DATA CLEANERS  ***************************************/
  /******************************************************************************************/
 
@@ -465,7 +503,7 @@ function join_select_where_order_by($join_arr, $where_arr, $order_by, $switch = 
     $query_string = perform_where($query_string, $where_arr);
     
     $query_string = perform_order_by($query_string, $order_by);
-
+	
     return clean_and_return_rows($conn, $query_string);
 }
 
@@ -699,7 +737,7 @@ function perform_join($join_arr, $items, $switch = null)
         {
             $main_table = $key;
             $on_column = $value;
-            $query_string .= "SELECT * FROM " . "`" . $main_table . "` ";
+            $query_string .= "SELECT * FROM " . "" . $main_table . " ";
         } else 
         {    
             $query_string .= "JOIN " . $key . " ON " . $key . "." . $value . "=" . $main_table . "." . $on_column . " ";
@@ -732,13 +770,13 @@ function perform_select($table, $items)
 
     foreach($items as $item)
     {
-        $query_string .= " " . $item . ",";
+        $query_string .= " " . clean_reserved_words($item) . ",";
     }
 
     //Remove last comma
     $query_string = substr_replace($query_string, '', -1);
 
-    return $query_string .= " FROM " . "`" . $table . "`";
+    return $query_string .= " FROM " . "" . clean_reserved_words($table) . "";
 }
 
 /**
@@ -753,7 +791,7 @@ function perform_where($query_string, $where_arr)
     $where_clauses = [];
     foreach($where_arr as $key => $value)
     {
-        $where_clauses[] = "`" . $key . "` = '" . $value . "'";
+        $where_clauses[] = "" . clean_reserved_words($key) . " = '" . clean_reserved_words($value) . "'";
     }
     if (!empty($where_clauses)) {
         $query_string .= " WHERE " . implode(' AND ', $where_clauses);
@@ -775,7 +813,7 @@ function perform_order_by($query_string, $order_by)
     //Creating the query
     foreach($order_by as $key => $value)
     {
-       $query_string .= "`" . $key . "` " . $value . ",";
+       $query_string .= "" . clean_reserved_words($key) . " " . $value . ",";
     }
     
     //Remove last comma
@@ -790,6 +828,7 @@ function perform_order_by($query_string, $order_by)
  * @return array An array of associative arrays, each representing a row of data.
  */
 function clean_and_return_rows($conn, $query_string) {
+    echo $query_string;
     $result = mysqli_query($conn, $query_string);
     $rows = array();
     while ($row = mysqli_fetch_array($result)) {
@@ -804,4 +843,17 @@ function clean_and_return_rows($conn, $query_string) {
     return $rows;
 }
 
+function clean_reserved_words($word)
+{
+    global $mysql_reserved_words;
+    if(in_array(strtoupper(trim($word)), $mysql_reserved_words))
+    {
+        return "'" . $word . "'";
+    }
+    else
+    {
+        return $word;
+    }
+    
+}
 
